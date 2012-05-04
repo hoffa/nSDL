bbf4556b1171f25e391e9d8a59738097c77e96a2
Christoffer Rehn <christoffer.rehn@telenet.be>
2012-05-04 19:35:21 +0200

    Fixed bpp problem with NTI image loading.

--- src/video/tinspire/SDL_tinspirenti.c
+++ src/video/tinspire/SDL_tinspirenti.c
@@ -24,7 +24,7 @@ static void nti_free_info(nti_info_t *nti_info)
 
 SDL_Surface *SDL_nLoadImage(Uint16 *data)
 {
-	SDL_Surface *image;
+	SDL_Surface *tmp, *image;
 	nti_info_t *nti_info = nti_get_info(data);
 	int i;
 	int length;
@@ -39,35 +39,24 @@ SDL_Surface *SDL_nLoadImage(Uint16 *data)
 	}
 	NSP_DPRINT("Loading NTI v%d (%dx%d)\n",
 		   nti_info->version, nti_info->width, nti_info->height);
-	image = SDL_CreateRGBSurface(SDL_SWSURFACE, nti_info->width, nti_info->height,
-				     NSP_BPP, NSP_RMASK, NSP_GMASK, NSP_BMASK, 0);
-	if ( image == NULL ) {
+	tmp = SDL_CreateRGBSurface(SDL_SWSURFACE, nti_info->width, nti_info->height,
+				   16, 0xF800, 0x7E0, 0x1F, 0);
+	if ( tmp == NULL ) {
 		SDL_OutOfMemory();
 		nti_free_info(nti_info);
 		return(NULL);
 	}
-#if NSP_BPP_SW8
-	if ( ! SDL_nCreatePalette(image) ) {
-		SDL_FreeSurface(image);
-		nti_free_info(nti_info);
-		return(NULL);
-	}
-#endif
 	data = (Uint16 *)(data + 4);
 	length = nti_info->width * nti_info->height;
-	SDL_LockSurface(image);
-	for ( i = 0; i < length; ++i ) {
-#if NSP_BPP_SW16
-		*(Uint16 *)(image->pixels + (2 * i)) = data[i];
-#else
-		Uint8 r, g, b;
-		r = 8 * (data[i] >> 11);
-		g = 4 * ((data[i] >> 5) & 0x3F);
-		b = 8 * (data[i] & 0x1F);
-		*(Uint8 *)(image->pixels + i) = SDL_MapRGB(image->format, r, g, b);
-#endif
-	}
-	SDL_UnlockSurface(image);
+	SDL_LockSurface(tmp);
+	for ( i = 0; i < length; ++i )
+		*(Uint16 *)(tmp->pixels + (2 * i)) = data[i];
+	SDL_UnlockSurface(tmp);
+	if(NSP_BPP == 8) {
+		image = SDL_DisplayFormat(tmp);
+		SDL_FreeSurface(tmp);
+	} else
+		image = tmp;
 	nti_free_info(nti_info);
 	return(image);
 }
---
+++ tinspire/test/tinspire_test__.c
@@ -0,0 +1,72 @@
+#include <os.h>
+#include <SDL.h>
+
+#define AMOUNT 10
+
+int main(void) {
+	SDL_Surface *screen;
+	SDL_nFont *font_vga;
+	int done = 0;
+	SDL_Event event;
+	SDL_Rect rect = {0, 0, 30, 30};
+	int derp = 0;
+
+	SDL_putenv("SDL_WARN_NOMOUSE=1");
+	SDL_putenv("SDL_USEMOUSE=1");
+	if(SDL_Init(SDL_INIT_VIDEO) == -1) {
+		printf("SDL_Init error: %s\n", SDL_GetError());
+		return 1;
+	}
+	screen = SDL_SetVideoMode(320, 240, NSP_BPP, SDL_SWSURFACE);
+	if(screen == NULL) {
+		printf("SDL_SetVideoMode error: %s\n", SDL_GetError());
+		return 1;
+	}
+	font_vga = SDL_nLoadFont(NSP_FONT_TINYTYPE, SDL_MapRGB(screen->format, 1, 0, 0), NSP_FONTCFG_DEFAULT);
+	SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
+	while(!done) {
+		SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255, 255));
+		//SDL_WaitEvent(&event);
+		while(SDL_PollEvent(&event))
+		switch(event.type) {
+			case SDL_MOUSEMOTION:
+				SDL_nDrawString(screen, font_vga, 10, 20, "Mouse: (%d,%d)\n", event.motion.x, event.motion.y);
+				++derp;
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+				SDL_nDrawString(screen, font_vga, 10, 30, "SDL_MOUSEBUTTONDOWN (%d,%d)", event.button.x, event.button.y);
+				break;
+			case SDL_KEYDOWN:
+				switch(event.key.keysym.sym) {
+					case SDLK_UP:
+						rect.y -= AMOUNT;
+						break;
+					case SDLK_DOWN:
+						rect.y += AMOUNT;
+						break;
+					case SDLK_LEFT:
+						rect.x -= AMOUNT;
+						break;
+					case SDLK_RIGHT:
+						rect.x += AMOUNT;
+						break;
+					case SDLK_ESCAPE:
+						done = 1;
+						break;
+					default:
+						break;
+				}
+				break;
+			default:
+				break;
+		}
+		SDL_FillRect(screen, &rect, 0);
+		SDL_nDrawString(screen, font_vga, 10, 10, NSP_NAME_FULL);
+		SDL_nDrawString(screen, font_vga, 10, 50, "%d", derp);
+		SDL_Flip(screen);
+	}
+	SDL_nFreeFont(font_vga);
+	SDL_Quit();
+
+	return 0;
+}
